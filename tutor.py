5247839b864a107641cb2c25ed35287119d012ee
Bingran You <bingran.you.us@gmail.com>
2024-12-28 19:12:37 -0800

    Update tutor.py

--- tutor.py
+++ tutor.py
@@ -98,11 +98,17 @@ if st.session_state['isAuth']:
 
             # Generate embeddings based on the selected mode
             if st.session_state.mode == "Professor":
-                with st.spinner("Processing file to generate knowledge graph"):
+                with st.spinner("Processing file to generate knowledge graph, may take 3 - 5 mins..."):
                     if(index_files_decompress(embedding_folder)):
                         print("Index files are ready.")
                     else:
-                        with st.spinner("Processing file to generate knowledge graph, may take 3 - 5 mins..."):
+                        # Files are missing and have been cleaned up
+                        save_file_locally(file, filename=uploaded_file.name, embedding_folder=embedding_folder)
+                        generate_embedding(documents, embedding_folder=embedding_folder)
+                        generate_GraphRAG_embedding(documents, embedding_folder=embedding_folder)
+                        if(index_files_compress(embedding_folder)):
+                            print("Index files are ready and uploaded to Azure Blob Storage.")
+                        else:
                             # Files are missing and have been cleaned up
                             save_file_locally(file, filename=uploaded_file.name, embedding_folder=embedding_folder)
                             generate_embedding(documents, embedding_folder=embedding_folder)
@@ -110,14 +116,7 @@ if st.session_state['isAuth']:
                             if(index_files_compress(embedding_folder)):
                                 print("Index files are ready and uploaded to Azure Blob Storage.")
                             else:
-                                # Files are missing and have been cleaned up
-                                save_file_locally(file, filename=uploaded_file.name, embedding_folder=embedding_folder)
-                                generate_embedding(documents, embedding_folder=embedding_folder)
-                                generate_GraphRAG_embedding(documents, embedding_folder=embedding_folder)
-                                if(index_files_compress(embedding_folder)):
-                                    print("Index files are ready and uploaded to Azure Blob Storage.")
-                                else:
-                                    print("Error compressing and uploading index files to Azure Blob Storage.")
+                                print("Error compressing and uploading index files to Azure Blob Storage.")
             else:
                 with st.spinner("Processing file..."):
                     generate_embedding(documents, embedding_folder=embedding_folder)

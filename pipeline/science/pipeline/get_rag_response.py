928bcd17620847a409c2aeb82f2601aab3de3ae3
Bingran You <bingran.you@berkeley.edu>
2025-03-09 22:55:36 -0700

    Update get_rag_response.py

--- pipeline/science/pipeline/get_rag_response.py
+++ pipeline/science/pipeline/get_rag_response.py
@@ -110,6 +110,7 @@ async def get_db_rag_response(
         if stream:
             def parsed_result_stream_response(chain, user_input, processed_chat_history):
                 yield "<response>"
+                chat_session.current_message += "<response>"
                 parsed_result = chain.stream({
                     "input": user_input,
                     "chat_history": processed_chat_history
@@ -120,7 +121,9 @@ async def get_db_rag_response(
                     chunk_content = chunk.get("answer", "")
                     print(chunk_content, end="", flush=True)
                     yield chunk_content
+                    chat_session.current_message += chunk_content
                 yield "</response>"
+                chat_session.current_message += "</response>"
             parsed_result = parsed_result_stream_response(chain, user_input, processed_chat_history)
         else:
             def parsed_result_invoke_response(chain, user_input, processed_chat_history):

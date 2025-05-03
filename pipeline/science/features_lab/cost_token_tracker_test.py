c758551d9aab02be4cd11835fb1027032dbf714a
Bingran You <bingran.you@berkeley.edu>
2025-05-03 15:46:38 -0700

    cost_token_tracker_test

--- pipeline/science/features_lab/cost_token_tracker_test.py
+++ pipeline/science/features_lab/cost_token_tracker_test.py
@@ -1,4 +1,5 @@
 from langchain_openai import AzureChatOpenAI
+from langchain_sambanova import ChatSambaNovaCloud
 from langchain_core.output_parsers import StrOutputParser
 from langchain.output_parsers import OutputFixingParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -21,11 +22,21 @@ llm = AzureChatOpenAI(
     model_kwargs={"stream_options": {"include_usage": True}}
 )
 
+llm = ChatSambaNovaCloud(
+    model="Meta-Llama-3.3-70B-Instruct",
+    api_key=os.getenv("SAMBANOVA_API_KEY"),
+    base_url="https://api.sambanova.ai/v1",
+    max_tokens=1024,
+    temperature=0,
+    top_p=0.1,
+    streaming=True
+)
+
 parser = StrOutputParser()
-error_parser = OutputFixingParser.from_llm(parser=parser, llm=llm)
+# error_parser = OutputFixingParser.from_llm(parser=parser, llm=llm)
 prompt = "what is 2 + 2 = ? explain the answer in great detail"
 prompt = ChatPromptTemplate.from_template(prompt)
-chain = prompt | llm | error_parser
+chain = prompt | llm | parser
 
 # Using the get_openai_callback context manager to track token usage and cost
 with get_openai_callback() as cb:
@@ -33,11 +44,11 @@ with get_openai_callback() as cb:
     print(type(response))
     # print(response)
     for chunk in response:
-        print(chunk, end="\n", flush=True)
+        print(chunk, end="", flush=True)
     
     # Print token usage and cost information
     print("\nToken Usage Statistics:")
     print(f"Prompt Tokens: {cb.prompt_tokens}")
     print(f"Completion Tokens: {cb.completion_tokens}")
     print(f"Total Tokens: {cb.total_tokens}")
-    print(f"Total Cost (USD): ${cb.total_cost:.6f}")
+    print(f"Total Cost (USD): ${cb.total_cost:.10f}")

9b5abdbcfabd22158de6e9ec830021cc94b9fe80
Bingran You <bingran.you@berkeley.edu>
2025-10-02 11:20:43 -0700

    Update sources_retrieval.py

--- pipeline/science/pipeline/sources_retrieval.py
+++ pipeline/science/pipeline/sources_retrieval.py
@@ -543,6 +543,8 @@ def get_response_source(
             user_input=user_input,
             divider_number=4,
         )
+        logger.info(f"\n\nTag {original_tag_symbol} mapped to page {page_num}, index {source_index}, content length {len(content_key)}")
+        logger.info(f"original tag string: {tag_string}:\nmost relevant chunk:\n{content_key}\n---\n")
 
         # -----------------------------------------------------------------
         # 2.c Assign / reuse new tag id
